869b4b02a7fa7bc98604bcb6849312f62e6c7d8a
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-09 17:53:47 -0400

    Inputs for axe

---
+++ Assets/Scripts/PlayerAxeManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class PlayerAxeManager : MonoBehaviour
+{
+    private int _currentAxeCount = 1;
+    private int _maxAxeCount = 1;
+    public bool HasAxe => _currentAxeCount > 0;
+
+    private bool _throwQueued = false;
+
+    [SerializeField] private InputActionReference swingAction;
+    [SerializeField] private InputActionReference throwAction;
+    [SerializeField] private InputActionReference recallAction;
+
+    private void Start()
+    {
+    }
+
+    public void ChangeAxeCount(int delta)
+    {
+        _currentAxeCount += delta;
+
+        if (_currentAxeCount < 0)
+            _currentAxeCount = 0;
+
+        if (_currentAxeCount > _maxAxeCount)
+            _currentAxeCount = _maxAxeCount;
+    }
+
+    private void OnEnable()
+    {
+        swingAction.action.Enable();
+        throwAction.action.Enable();
+        recallAction.action.Enable();
+
+        swingAction.action.performed += OnSwingPerformed;
+        throwAction.action.performed += OnThrowPrepared;
+        throwAction.action.canceled += OnThrowReleased;
+        recallAction.action.started += OnRecallPerformed;
+    }
+
+    private void OnThrowReleased(InputAction.CallbackContext obj)
+    {
+        if (_throwQueued){
+            _throwQueued = false;
+            Debug.Log("Axe Thrown.");
+        }
+    }
+
+    private void OnSwingPerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Swing performed");
+    }
+
+    private void OnThrowPrepared(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Throw prepared");
+        _throwQueued = true;
+    }
+
+    private void OnRecallPerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Recall performed");
+    }
+
+
+    private void OnDisable()
+    {
+        swingAction.action.Disable();
+        throwAction.action.Disable();
+        recallAction.action.Disable();
+    }
+}
