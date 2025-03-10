d249e3239b6426ed8d6708269ec8036c2854db3b
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-10 00:24:28 -0400

    Update AxeProjectile.cs

--- Assets/Scripts/AxeProjectile.cs
+++ Assets/Scripts/AxeProjectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Content.Scripts.Components;
 using UnityEngine;
 
 public class AxeProjectile : ProjectileComponent
@@ -8,5 +9,6 @@ public class AxeProjectile : ProjectileComponent
     {
         base.OnCollisionEnter(collision);
         rb.isKinematic = true;
+        GetComponentInChildren<InteractorComponent>().DeactivateInteractable();
     }
 }
