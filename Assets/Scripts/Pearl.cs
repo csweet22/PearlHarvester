c3a996663e6143ec4e9883cb1d44a7ab51978f93
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-10 19:44:27 -0400

    Basic Pearl

---
+++ Assets/Scripts/Pearl.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pearl : MonoBehaviour
+{
+    private TriggerComponent _triggerComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _triggerComponent = gameObject.GetComponentInChildren<TriggerComponent>();
+        _triggerComponent.TriggerEnter += OnTriggerEnter;
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        Tween lerp = DOTween.To(() => gameObject.transform.position, x => gameObject.transform.position = x,
+            PlayerCore.Instance.PlayerPosition, 0.2f);
+        lerp.onComplete += () => { Destroy(gameObject); };
+    }
+}
