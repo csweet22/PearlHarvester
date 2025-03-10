2e1e5e578eb517cfe2f81815c5c8de635dd123b8
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-10 18:24:12 -0400

    Teleporters

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private TriggerComponent triggerComponent;
+    [SerializeField] private Transform target;
+
+    private void Start()
+    {
+        triggerComponent.TriggerEnter += Teleport;
+    }
+
+    private void Teleport(Collider col)
+    {
+        Rigidbody rb = col.GetComponentInParent<Rigidbody>();
+        rb.gameObject.transform.position = target.position;
+    }
+}
