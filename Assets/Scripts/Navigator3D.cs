3d999837da4b61e90e0122fc0962682ae7af03f7
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-10 23:05:13 -0400

    Something 🤔

--- Assets/Scripts/Navigator3D.cs
+++ Assets/Scripts/Navigator3D.cs
@@ -11,7 +11,13 @@ public class Navigator3D : MonoBehaviour
     {
         public Vector3Int GridPosition;
         public Vector3 WorldPosition;
-        public float DistanceToTarget = Mathf.Infinity;
+        public float DistanceToDest = Mathf.Infinity;
+        public Sample Up = null;
+        public Sample Down = null;
+        public Sample Left = null;
+        public Sample Right = null;
+        public Sample Forward = null;
+        public Sample Back = null;
 
         public Sample(Vector3Int gridPosition, Vector3 worldPosition)
         {
@@ -28,10 +34,11 @@ public class Navigator3D : MonoBehaviour
 
     [SerializeField] private int updateFrequency = 5;
 
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform src;
+    [SerializeField] private Transform dest;
 
     public int tick = 0;
-    
+
     private void OnValidate()
     {
         Init();
@@ -40,7 +47,7 @@ public class Navigator3D : MonoBehaviour
     private void Init()
     {
         _grid.Clear();
-        
+
         int halfResolution = resolution / 2;
 
         for (int x = -halfResolution; x < halfResolution; x++){
@@ -49,7 +56,43 @@ public class Navigator3D : MonoBehaviour
                     Vector3Int gridPos = new Vector3Int(x, y, z);
                     Vector3 worldPos = transform.position + ((Vector3) gridPos * scale);
                     Sample newSample = new Sample(gridPos, worldPos);
+
                     _grid.Add(gridPos, newSample);
+                }
+            }
+        }
+
+        for (int x = -halfResolution; x < halfResolution; x++){
+            for (int y = -halfResolution; y < halfResolution; y++){
+                for (int z = -halfResolution; z < halfResolution; z++){
+                    Vector3Int gridPos = new Vector3Int(x, y, z);
+                    Sample sample = _grid[gridPos];
+
+                    if (gridPos.y + 1 < halfResolution){
+                        sample.Up = _grid[gridPos + Vector3Int.up];
+                    }
+
+                    if (gridPos.y - 1 > -halfResolution){
+                        sample.Down = _grid[gridPos + Vector3Int.down];
+                    }
+
+                    if (gridPos.x + 1 < halfResolution){
+                        sample.Right = _grid[gridPos + Vector3Int.right];
+                    }
+
+                    if (gridPos.x - 1 > -halfResolution){
+                        sample.Left = _grid[gridPos + Vector3Int.left];
+                    }
+
+                    if (gridPos.z + 1 < halfResolution){
+                        sample.Forward = _grid[gridPos + Vector3Int.forward];
+                    }
+
+                    if (gridPos.z - 1 > -halfResolution){
+                        sample.Back = _grid[gridPos + Vector3Int.back];
+                    }
+
+                    _grid[gridPos] = sample;
                 }
             }
         }
@@ -72,31 +115,29 @@ public class Navigator3D : MonoBehaviour
     [ContextMenu("Recalculate Path")]
     private void RecalculatePath()
     {
-        
-        if (!target)
-            return;
+        if (!src || !dest) return;
 
+        Stack<Vector3Int> path = new Stack<Vector3Int>();
+        
         Sample closestToTarget;
         float closestDistance = Mathf.Infinity;
-        
+
         foreach (KeyValuePair<Vector3Int, Sample> kvp in _grid){
-            kvp.Value.DistanceToTarget = Vector3.Distance(kvp.Value.WorldPosition, target.position);
-            if (kvp.Value.DistanceToTarget < closestDistance){
-                closestDistance = kvp.Value.DistanceToTarget;
+            kvp.Value.DistanceToDest = Vector3.Distance(kvp.Value.WorldPosition, dest.position);
+            if (kvp.Value.DistanceToDest < closestDistance){
+                closestDistance = kvp.Value.DistanceToDest;
                 closestToTarget = kvp.Value;
             }
+
             Debug.Log(closestDistance);
         }
-        
-        
-        
     }
 
     private void OnDrawGizmos()
     {
         foreach (KeyValuePair<Vector3Int, Sample> kvp in _grid){
             Sample sample = kvp.Value;
-            Gizmos.color =  Color.Lerp(Color.red, Color.green, sample.DistanceToTarget / (scale * resolution / 2));
+            Gizmos.color = Color.Lerp(Color.red, Color.green, sample.DistanceToDest / (scale * resolution / 2));
             Gizmos.DrawSphere(sample.WorldPosition, 0.4f);
         }
     }
