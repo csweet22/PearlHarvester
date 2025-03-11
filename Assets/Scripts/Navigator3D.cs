e1596d88a4d6b9e3c002a07ebfae304ddd135667
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-10 22:50:34 -0400

    Beginnings of pathfinding

---
+++ Assets/Scripts/Navigator3D.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Navigator3D : MonoBehaviour
+{
+    private Dictionary<Vector3Int, Sample> _grid = new Dictionary<Vector3Int, Sample>();
+
+    private class Sample
+    {
+        public Vector3Int GridPosition;
+        public Vector3 WorldPosition;
+        public float DistanceToTarget = Mathf.Infinity;
+
+        public Sample(Vector3Int gridPosition, Vector3 worldPosition)
+        {
+            GridPosition = gridPosition;
+            WorldPosition = worldPosition;
+        }
+    }
+
+    // 8 by 8 by 8 grid
+    [SerializeField] private int resolution = 8;
+
+    // Amount of space between samples
+    [SerializeField] private float scale = 1f;
+
+    [SerializeField] private int updateFrequency = 5;
+
+    [SerializeField] private Transform target;
+
+    public int tick = 0;
+    
+    private void OnValidate()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _grid.Clear();
+        
+        int halfResolution = resolution / 2;
+
+        for (int x = -halfResolution; x < halfResolution; x++){
+            for (int y = -halfResolution; y < halfResolution; y++){
+                for (int z = -halfResolution; z < halfResolution; z++){
+                    Vector3Int gridPos = new Vector3Int(x, y, z);
+                    Vector3 worldPos = transform.position + ((Vector3) gridPos * scale);
+                    Sample newSample = new Sample(gridPos, worldPos);
+                    _grid.Add(gridPos, newSample);
+                }
+            }
+        }
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        tick++;
+        if (tick >= updateFrequency){
+            tick = 0;
+            RecalculatePath();
+        }
+    }
+
+    [ContextMenu("Recalculate Path")]
+    private void RecalculatePath()
+    {
+        
+        if (!target)
+            return;
+
+        Sample closestToTarget;
+        float closestDistance = Mathf.Infinity;
+        
+        foreach (KeyValuePair<Vector3Int, Sample> kvp in _grid){
+            kvp.Value.DistanceToTarget = Vector3.Distance(kvp.Value.WorldPosition, target.position);
+            if (kvp.Value.DistanceToTarget < closestDistance){
+                closestDistance = kvp.Value.DistanceToTarget;
+                closestToTarget = kvp.Value;
+            }
+            Debug.Log(closestDistance);
+        }
+        
+        
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (KeyValuePair<Vector3Int, Sample> kvp in _grid){
+            Sample sample = kvp.Value;
+            Gizmos.color =  Color.Lerp(Color.red, Color.green, sample.DistanceToTarget / (scale * resolution / 2));
+            Gizmos.DrawSphere(sample.WorldPosition, 0.4f);
+        }
+    }
+}
