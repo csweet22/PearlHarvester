0902b6281ca26e6378f82cf0dd6368f735dc892e
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-11 16:12:52 -0400

    Update MainCanvas.cs

--- Assets/Scripts/MainCanvas.cs
+++ Assets/Scripts/MainCanvas.cs
@@ -11,20 +11,17 @@ public class MainCanvas : PersistentSingleton<MainCanvas>
     [SerializeField] private Transform rootMenu;
     [SerializeField] private GameObject mainMenu;
 
-    private Stack<ACMenu> menuStack = new Stack<ACMenu>();
-    private Stack<Vector3> offsets = new Stack<Vector3>();
+    private readonly Stack<ACMenu> _menuStack = new Stack<ACMenu>();
+    private readonly Stack<Vector3> _offsets = new Stack<Vector3>();
 
     private Canvas _canvas;
-    private float _width => _canvas.pixelRect.width;
-    private float _height => _canvas.pixelRect.height;
-
-    [SerializeField] private Vector3 slideFrom;
-
-    [ContextMenu("Show Main Menu")]
+    private float Width => _canvas.pixelRect.width;
+    private float Height => _canvas.pixelRect.height;
+    
     private void Start()
     {
         _canvas = GetComponent<Canvas>();
-        OpenMenu(mainMenu, slideFrom, 1.0f);
+        OpenMenu(mainMenu, Vector3.zero, 0.0f);
     }
 
     public void OpenMenu(GameObject menuObject, Vector3 slideFrom = new Vector3(), float duration = 0.2f)
@@ -41,43 +38,42 @@ public class MainCanvas : PersistentSingleton<MainCanvas>
 
         menu.transform.SetParent(rootMenu);
 
-        Vector3 offset = new Vector3(slideFrom.x * _width, slideFrom.y * _height, slideFrom.z);
+        Vector3 offset = new Vector3(slideFrom.x * Width, slideFrom.y * Height, slideFrom.z);
         menu.Open();
 
         // Disable current head
-        if (menuStack.Count > 0){
-            ACMenu currentHead = menuStack.Peek();
+        if (_menuStack.Count > 0){
+            ACMenu currentHead = _menuStack.Peek();
             currentHead.enabled = false;
             menu.transform.localPosition = currentHead.transform.localPosition + offset;
         }
         else{
             menu.transform.localPosition = Vector3.zero;
-            offsets.Push(Vector3.zero);
+            _offsets.Push(Vector3.zero);
         }
 
-        if (menuStack.Count > 0){
-            offsets.Push(offset);
+        if (_menuStack.Count > 0){
+            _offsets.Push(offset);
             Vector3 targetPosition = rootMenu.localPosition - offset;
             DOTween.To(() => rootMenu.localPosition, x => rootMenu.localPosition = x, targetPosition, duration)
                 .SetEase(Ease.InOutCubic);
         }
 
-        menuStack.Push(menu);
+        _menuStack.Push(menu);
     }
 
-    [ContextMenu("Close Menu")]
-    public void CloseMenu()
+    public void CloseMenu(float duration = 0.2f)
     {
-        if (menuStack.Count == 0){
+        if (_menuStack.Count == 0){
             return;
         }
 
-        ACMenu currentMenu = menuStack.Pop();
-        Vector3 offset = offsets.Pop();
+        ACMenu currentMenu = _menuStack.Pop();
+        Vector3 offset = _offsets.Pop();
 
-        if (menuStack.Count != 0){
+        if (_menuStack.Count != 0){
             Vector3 targetPosition = rootMenu.localPosition + offset;
-            DOTween.To(() => rootMenu.localPosition, x => rootMenu.localPosition = x, targetPosition, 1.0f)
+            DOTween.To(() => rootMenu.localPosition, x => rootMenu.localPosition = x, targetPosition, duration)
                 .SetEase(Ease.InOutCubic).onComplete = () => { currentMenu.Close(); };
         }
         else{
@@ -85,8 +81,8 @@ public class MainCanvas : PersistentSingleton<MainCanvas>
         }
 
 
-        if (menuStack.Count > 0){
-            ACMenu newMenu = menuStack.Peek();
+        if (_menuStack.Count > 0){
+            ACMenu newMenu = _menuStack.Peek();
             newMenu.enabled = true;
         }
     }
