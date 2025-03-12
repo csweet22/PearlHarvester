d99cd529ad7283136eed37f1044a929b12e437ca
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-12 00:08:36 -0400

    Pearl area start

---
+++ Assets/Scripts/PearlArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PearlArea : MonoBehaviour
+{
+    [SerializeField] private List<Pearl> pearlObjects = new List<Pearl>();
+
+    [SerializeField] private float percentRequired = 0.75f;
+
+    private int totalCount = 0;
+    
+    private void Start()
+    {
+        totalCount = pearlObjects.Count;
+    }
+
+    private void CheckThreshold()
+    {
+        if (pearlObjects.Count == 0) return;
+    }
+    
+}
