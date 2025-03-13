3e6d4629335b2452e306fc14e60b8081778aa0c1
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-13 13:39:49 -0400

    Audio and VFX managers

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scripts.Utilities;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public void SpawnVFX(AudioClip clip, float duration = 1.0f)
+    {
+        GameObject obj = new GameObject();
+        obj.transform.SetParent(transform);
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
+        StartCoroutine(
+            DelayedDestroy(obj, duration));
+    }
+
+    IEnumerator DelayedDestroy(GameObject obj, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(obj);
+    }
+}
---
+++ Assets/Scripts/VFXManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Scripts.Utilities;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXManager : Singleton<VFXManager>
+{
+    public void SpawnVFX(VisualEffectAsset vfxAsset, float duration = 1.0f, Vector3 position = default(Vector3))
+    {
+        GameObject obj = new GameObject
+        {
+            transform =
+            {
+                position = position
+            }
+        };
+        obj.transform.SetParent(transform);
+        VisualEffect effect = obj.AddComponent<VisualEffect>();
+        effect.visualEffectAsset = vfxAsset;
+        effect.Play();
+        StartCoroutine(
+            DelayedDestroy(obj, duration));
+    }
+
+    IEnumerator DelayedDestroy(GameObject obj, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(obj);
+    }
+}
