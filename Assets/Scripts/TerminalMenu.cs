30768a787730b634560b266e89e96aa24237b440
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-14 15:33:36 -0400

    Update TerminalMenu.cs

--- Assets/Scripts/TerminalMenu.cs
+++ Assets/Scripts/TerminalMenu.cs
@@ -68,8 +68,16 @@ public class TerminalMenu : ACMenu
                 ;
             }
             else{
-                upgradeText.text +=
-                    $"{requiredCount - GameManager.Instance.PearlCount.Value} MORE PEARLS UNTIL <b>UPGRADE {i + 1}</b>\n";
+                if (i == 0){
+                    upgradeText.text +=
+                        $"{requiredCount - GameManager.Instance.PearlCount.Value} MORE PEARLS UNTIL <b>AXE THROW SPEED</b>\n";
+                    
+                }
+                if (i == 1){
+                    upgradeText.text +=
+                        $"{requiredCount - GameManager.Instance.PearlCount.Value} MORE PEARLS UNTIL <b>CAN BREAK BONE DOORS</b>\n";
+                    
+                }
             }
         }
 
