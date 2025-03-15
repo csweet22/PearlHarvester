def54f16b2e43ed2b231a47b349fc9bcfa456b7d
Cole Sweet <94721502+csweet22@users.noreply.github.com>
2025-03-14 21:23:33 -0400

    Yep

---
+++ Assets/Scripts/Messager.cs
@@ -0,0 +1,16 @@
+﻿using Scripts.Utilities;
+using UnityEngine;
+
+public class Messager : Singleton<Messager>
+{
+    
+    [SerializeField] private GameObject prefab;
+    
+    public void Say(string message)
+    {
+        Transform parent = MainCanvas.Instance.transform;
+        GameObject obj = Instantiate(prefab, parent);
+        Message msg = obj.GetComponent<Message>();
+        msg.SetText(message);
+    }
+}
